356b01cefb99c9ee3e7c7aab23f110316537d081
Fasil Malik Hayat <fmh@forca.dk>
2024-01-05 15:44:34 +0100

    Opdateret

--- Install.ps1
+++ Install.ps1
@@ -1,8 +1,8 @@
 # Create Application1 console-app1 image
-docker build -f Corporate.Systems.Apllications.Application1\Dockerfile --force-rm -t console-app1-demo .
+docker build -f Corporate.Systems.Apllications.Application1\Docker\Dockerfile --force-rm -t console-app1-demo .
 
 # Create Application2 console-app2 image
-docker build -f Corporate.Systems.Apllications.Application2\Dockerfile --force-rm -t console-app2-demo .
+docker build -f Corporate.Systems.Apllications.Application2\Docker\Dockerfile --force-rm -t console-app2-demo .
 
 # Compose bundled containers
 docker compose -f Corporate.Systems.Apllications.Application1\Docker\docker-compose.yml -p app1 up -d --build 
