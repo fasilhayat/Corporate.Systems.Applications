18936ba2536f22beaadd3db79b6a55c83a3e2bed
Fasil Malik Hayat <fasil.hayat@gmail.com>
2024-01-09 23:21:22 +0100

    Script updates

--- Install.ps1
+++ Install.ps1
@@ -1,7 +1,5 @@
-# Create Application1 console-app1 image
+# Create Application1 console-app1 image and Application2 console-app2 image
 docker build -f Corporate.Systems.Applications.Application1\Docker\Dockerfile --force-rm -t console-app1-demo .
-
-# Create Application2 console-app2 image
 docker build -f Corporate.Systems.Applications.Application2\Docker\Dockerfile --force-rm -t console-app2-demo .
 
 # Create Application4 rest-app4 image
@@ -9,10 +7,6 @@ docker build -f Corporate.Systems.Applications.Application4\Docker\Dockerfile --
 
 # Compose bundled containers
 docker compose -f Corporate.Systems.Applications.Application1\Docker\docker-compose.yml -p app1 up -d --build 
-
-# Compose bundled containers
 docker compose -f Corporate.Systems.Applications.Application2\Docker\docker-compose.yml -p app2 up -d --build
-
-# Compose bundled containers
 docker compose -f Corporate.Systems.Applications.Application4\Docker\docker-compose.yml -p app4 up -d --build
 
