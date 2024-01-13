01b00766da379f18f2d9c39544b8558814f15680
Fasil Malik Hayat <fasil.hayat@gmail.com>
2024-01-13 07:48:12 +0100

    Updated

---
+++ Corporate.Systems.Applications.Application5/Redis.Adapter/DataAccess.cs
@@ -0,0 +1,49 @@
+﻿namespace Corporate.Systems.Applications.Application5.Redis.Adapter;
+
+using Common;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+internal class DataAccess : IDataAccess
+{
+    private readonly IDistributedCache _cache;
+    private readonly DistributedCacheEntryOptions _cachePolicy;
+
+    public DataAccess(IDistributedCache cache, DistributedCacheEntryOptions cachePolicy)
+    {
+        _cache = cache;
+        _cachePolicy = cachePolicy;
+    }
+
+    public void Delete(IDataKey key)
+    {
+        // Remove datakey from datastore
+        //ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("127.0.0..1:6379");
+        //var server = redis.GetServer("127.0.0..1:6379");
+        this.ClearData(key);
+    }
+
+    public T? GetData<T>(IDataKey dataKey) where T : class
+    {
+        var document = _cache.Get(dataKey.Identifier);
+        var cahchedData = document != null ? JsonSerializer.Deserialize<T>(document) : null;
+        return cahchedData;
+    }
+
+    public T SaveData<T>(IDataKey key, T o) where T : class
+    {
+        var serializedData = JsonSerializer.SerializeToUtf8Bytes(o);
+        _cache.Set(key.Identifier, serializedData, _cachePolicy);
+        return o;
+    }
+
+    public void ClearData(IDataKey key)
+    {
+        _cache.Remove(key.Identifier);
+    }
+
+    public void ClearAllData()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Corporate.Systems.Applications.Application5/Redis.Adapter/IDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using Corporate.Systems.Applications.Common;
+
+namespace Corporate.Systems.Applications.Application5.Redis.Adapter;
+
+public interface IDataAccess : IDeletionPolicy
+{
+    T? GetData<T>(IDataKey key) where T : class;
+
+    T SaveData<T>(IDataKey key, T o) where T : class;
+
+    void ClearData(IDataKey key);
+
+    void ClearAllData();
+}
