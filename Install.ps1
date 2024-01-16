5423547b888629af53e09fbfa874f82eae3f9271
Fasil Malik Hayat <fmh@forca.dk>
2024-01-16 12:07:37 +0100

    Full docker spinning

--- Install.ps1
+++ Install.ps1
@@ -1,15 +1,15 @@
 # Create Application1 console-app1 image and Application2 console-app2 image
-#docker build -f Corporate.Systems.Applications.Application1\Docker\Dockerfile --force-rm -t console-app1-demo .
-#docker build -f Corporate.Systems.Applications.Application2\Docker\Dockerfile --force-rm -t console-app2-demo .
+docker build -f Corporate.Systems.Applications.Application1\Docker\Dockerfile --force-rm -t console-app1-demo .
+docker build -f Corporate.Systems.Applications.Application2\Docker\Dockerfile --force-rm -t console-app2-demo .
 
 # Create Application4 rest-app4 image
-#docker build -f Corporate.Systems.Applications.Application4\Docker\Dockerfile --force-rm -t rest-app4-demo .
+docker build -f Corporate.Systems.Applications.Application4\Docker\Dockerfile --force-rm -t rest-app4-demo .
 
 # Create Application5 graphql-app5 image
 docker build -f Corporate.Systems.Applications.Application5\Docker\Dockerfile --force-rm -t graphql-app5-demo .
 
 # Compose bundled containers
-#docker compose -f Corporate.Systems.Applications.Application1\Docker\docker-compose.yml -p app1 up -d --build 
-#docker compose -f Corporate.Systems.Applications.Application2\Docker\docker-compose.yml -p app2 up -d --build
-#docker compose -f Corporate.Systems.Applications.Application4\Docker\docker-compose.yml -p app4 up -d --build
+docker compose -f Corporate.Systems.Applications.Application1\Docker\docker-compose.yml -p app1 up -d --build 
+docker compose -f Corporate.Systems.Applications.Application2\Docker\docker-compose.yml -p app2 up -d --build
+docker compose -f Corporate.Systems.Applications.Application4\Docker\docker-compose.yml -p app4 up -d --build
 docker compose -f Corporate.Systems.Applications.Application5\Docker\docker-compose.yml -p app5 up -d --build
--- Uninstall.ps1
+++ Uninstall.ps1
@@ -1,18 +1,18 @@
 # STOP AND REMOVE APP1 CONTAINERS
-#docker stop app1-app1-1
-#docker stop app1-cache-1
-#docker rm -f app1-app1-1
-#docker rm -f app1-cache-1
+docker stop app1-app1-1
+docker stop app1-cache-1
+docker rm -f app1-app1-1
+docker rm -f app1-cache-1
 
 # STOP AND REMOVE APP2 CONTAINERS
-#docker stop app2-app2-1
-#docker stop app2-mongo-1
-#docker rm -f app2-app2-1
-#docker rm -f app2-mongo-1
+docker stop app2-app2-1
+docker stop app2-mongo-1
+docker rm -f app2-app2-1
+docker rm -f app2-mongo-1
 
 # STOP AND REMOVE APP4 CONTAINERS
-#docker stop app4-app4-1
-#docker rm -f app4-app4-1
+docker stop app4-app4-1
+docker rm -f app4-app4-1
 
 # STOP AND REMOVE APP5 CONTAINERS
 docker stop app5-app5-1
@@ -21,9 +21,9 @@ docker rm -f app5-app5-1
 docker rm -f app5-cache-1
 
 # REMOVE IMAGES
-#docker rmi console-app1-demo
-#docker rmi console-app2-demo
-#docker rmi rest-app4-demo
+docker rmi console-app1-demo
+docker rmi console-app2-demo
+docker rmi rest-app4-demo
 docker rmi graphql-app5-demo
 
 # REMOVE ALL UNUSED VOLUMES
