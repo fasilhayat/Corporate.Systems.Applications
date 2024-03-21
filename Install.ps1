4cf387dcb1b7cdb851f77d75fc872f0db3dce87a
Fasil Malik Hayat <fmh@forca.dk>
2024-03-21 10:41:57 +0100

    Kørende eksempel

--- Install.ps1
+++ Install.ps1
@@ -5,7 +5,7 @@
 docker build -f Corporate.Systems.Applications.Application4\Docker\Dockerfile --force-rm -t rest-app4-demo .
 
 # Create Application5 graphql-app5 image
-#docker build -f Corporate.Systems.Applications.Application5\Docker\Dockerfile --force-rm -t graphql-app5-demo .
+docker build -f Corporate.Systems.Applications.Application5\Docker\Dockerfile --force-rm -t graphql-app5-demo .
 
 # Create Master console-master image
 #docker build -f Corporate.Systems.Applications.Master\Docker\Dockerfile --force-rm -t console-master-demo .
@@ -13,5 +13,5 @@ docker build -f Corporate.Systems.Applications.Application4\Docker\Dockerfile --
 # Compose bundled containers
 #docker compose -f Corporate.Systems.Applications.Application1\Docker\docker-compose.yml -p app1 up -d --build 
 docker compose -f Corporate.Systems.Applications.Application4\Docker\docker-compose.yml -p app4 up -d --build
-#docker compose -f Corporate.Systems.Applications.Application5\Docker\docker-compose.yml -p app5 up -d --build
+docker compose -f Corporate.Systems.Applications.Application5\Docker\docker-compose.yml -p app5 up -d --build
 #docker compose -f Corporate.Systems.Applications.Master\Docker\docker-compose.yml -p master up -d --build
